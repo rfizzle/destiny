d6d5e24fc14e6c83f27f9f4ee6ed55e1fde6baec
Coleton Pierson <coleton.pierson@praetorian.com>
2017-09-20 14:48:02 +0700

    Updated spec_helper config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,17 +16,8 @@
 require 'simplecov'
 require 'codecov'
 
-# save to CircleCI's artifacts directory if we're on CircleCI
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
-end
-
 # Set formatters
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::Codecov
-]
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 # Start SimpleCov
 SimpleCov.start 'rails'
