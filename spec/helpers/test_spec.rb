54a68ddb940dd5c9c347533dd15dc91e82cbfe31
Coleton Pierson <coleton.pierson@praetorian.com>
2017-09-20 15:16:05 +0700

    Removed unneeded tests.

--- spec/helpers/test_spec.rb
+++
@@ -1,6 +0,0 @@
-describe 'Test' do
-  it 'returns 4' do
-    x = 4
-    expect(x).to eql(4)
-  end
-end
--- spec/views/health/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'health/index.html.erb', type: :feature do
-  scenario 'returns a healthy redis check' do
-    visit '/health'
-    expect(page).to have_css('h1', text: 'Health#index')
-  end
-end
