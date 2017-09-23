70d39a826078ce02a205ce99fae4ee63026af74a
Coleton Pierson <coleton.pierson@praetorian.com>
2017-09-23 14:11:16 +0700

    Fixed RuboCop code quality issues.

--- spec/views/health/index.html.erb_spec.rb
+++ spec/views/health/index.html.erb_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'health/index.html.erb', type: :feature do
-    scenario 'route works' do
-      visit health_index_path
-      expect(current_path).to eq(health_index_path)
-    end
+  scenario 'route works' do
+    visit health_index_path
+    expect(current_path).to eq(health_index_path)
+  end
 end
