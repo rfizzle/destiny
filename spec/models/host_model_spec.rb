c4cb2f99f8fe236b51095e38681ae2f55e4e1549
Coleton Pierson <coleton.pierson@praetorian.com>
2017-09-23 15:23:53 +0700

    Updated host model spec.

--- spec/models/host_model_spec.rb
+++ spec/models/host_model_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Host, type: :model do
-  it('initializes and persists to the database') { Host.new.save }
+  it { is_expected.to be_mongoid_document }
+  it { is_expected.to have_field(:ip).of_type(String) }
+  it { is_expected.to have_field(:mac).of_type(String) }
+  it { is_expected.to have_field(:host_names).of_type(Array) }
+  it { is_expected.to have_field(:operating_system).of_type(String) }
+  it { is_expected.to have_field(:cpe).of_type(String) }
+  it { is_expected.to have_many(:ports).of_type(Port) }
 end
