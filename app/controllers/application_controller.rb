60f3741a545c7b07164494cb984f793949649d9a
Coleton Pierson <coleton.pierson@praetorian.com>
2017-10-25 18:24:29 -0500

    Updated application controller to utilize route helpers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include RouteHelpers
+
+  # Set the default run options used in all Trailblazer operations.
+  #
+  # For now, we initialize request IP and session.
+  #
+  def _run_options(options)
+    options.merge(request_ip: request.ip, session: session, current_user: current_user)
+  end
+
+  # Get the current user from the session
+  #
+  # @return [User,nil] returns a user object if a user is authenticated
+  def current_user
+    return nil unless session && session[:id]
+    {}
+  end
 end
